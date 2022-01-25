85a7baf2ca5358d646b5c8ab3e4c69c376f6acab
Matthew Cockcroft <94088344+RearedCape8319@users.noreply.github.com>
2022-01-25 23:32:17 +0000

    Got a noise field showing, needs refactoring so field is object

--- public/demos/marching/marching.js
+++ public/demos/marching/marching.js
@@ -1,46 +1,46 @@
 /* Declare global variables */
-let tOff, tInc;
-let grid, nodeCount, nRes;
+let res, len;
+let size, offset;
+let nScl, tVal, tInc;
 
 
-/* Setup function to run once before main loop */
+
+/* Setup function to be ran once at program start */
 function setup() {
-  // Setup canvas and settings
+  // Setup canvas
   let posinfo = document.getElementById("sketch-holder").getBoundingClientRect();
-  let canvas = createCanvas(posinfo.width, posinfo.height, P2D);
+  let canvas = createCanvas(posinfo.width, posinfo.height);
   canvas.parent("sketch-holder");
-  rectMode(CORNER);
-  colorMode(RGB, 255, 255, 255, 100);
+  rectMode(CENTER);
   // colorMode(HSB, 360, 100, 100, 100);
+  colorMode(RGB, 255, 255, 255, 255);
 
   // Initialise global variables
-  tOff = 0;
-  tInc = 0.01;
-  nOff = 0;
-  nInc = 0.01;
-  nodeCount = 50;
-  nRes = 15 / nodeCount;
-  grid = new Grid(width, height, nodeCount);
+  res = 60;
+  size = min(width, height) * 0.9;
+  offset = createVector((width-size)/2, (height-size)/2);
+  len = size / res;
+  nScl = 0.008;
+  tVal = 0;
+  tInc = 0.005;
 }
 
 
-/* Draw function is the main loop that runs continuously until program ends */
+
+/* Draw function to be ran continuously after setup funciton */
 function draw() {
-  background(120);
-  // background(50);
-  grid.show();
-  tOff += tInc;
+  // Draw the noise values
+  noStroke();
+  for (let i = 0; i < res; i++) {
+    for (let j = 0; j < res; j++) {
+      let x = i*len + offset.x;
+      let y = j*len + offset.y;
+      // fill(noise(x, y) * 360, 100, 100);
+      fill(noise(x*nScl, y*nScl, tVal) * 255);
+      rect(x, y, len, len);
+    }
+  }
+
+  // Increment time
+  tVal += tInc;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-// PLacehldere
