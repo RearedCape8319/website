a62a8c392974065c765648665914ea70431384ab
Matthew Cockcroft <94088344+RearedCape8319@users.noreply.github.com>
2022-04-07 04:33:40 +0100

    Inefficient but working

---
+++ public/demos/circles/circles.js
@@ -0,0 +1,131 @@
+/**
+ * DECLARE GLOBAL VARIABLES
+ * - Noise field for every pixel and noise increment
+ * - The array of circles
+ * - Max attempts for making new circles and max count of circles
+ * - Cat image and width and height
+ **/
+let noiseField, nInc, nSize;
+let circles, circleCount;
+let maxAttempts, maxCircles;
+let catImg, catW, catH, catScl, catOffset;
+
+
+
+/**
+ * PRELOAD FUNCTION RUNS ONCE BEFORE SETUP
+ * - Load the cat image
+ **/
+function preload() {
+  catImg = loadImage("../../images/cat.jpg");
+}
+
+
+
+ /**
+  * SETUP FUNCTION RUNS ONCE AT PROGRAM START
+  * - Setup canvas and settings
+  * - Initialise global variables
+  **/
+function setup() {
+
+  // Create canvas
+  let posinfo = document.getElementById("sketch-holder").getBoundingClientRect();
+  let canvas = createCanvas(posinfo.width, posinfo.height);
+  canvas.parent("sketch-holder");
+
+  // Setup graphic settings
+  // colorMode(HSB, 360, 100, 100, 100);
+  colorMode(RGB, 255, 255, 255, 100);
+  ellipseMode(CENTER);
+  rectMode(CORNER);
+
+  // Initialise noise field
+  nInc = 0.2;
+  nSize = 10;
+  noiseField = [];
+  for (let i = 0; i < height/nSize; i++) {
+    let row = [];
+    for (let j = 0; j < width/nSize; j++) {
+      row.push(noise(j*nInc, i*nInc));
+    }
+    noiseField.push([...row]);
+  }
+
+  // Initialise cat image dimensions
+  catScl = min(height/catImg.height, width/catImg.width);
+  catW = floor(catImg.width * catScl);
+  catH = floor(catImg.height * catScl);
+  catImg.resize(catW, catH);
+  catOffset = createVector((width - catW)/2, (height - catH)/2);
+
+  // Initialise circles
+  maxAttempts = 50;
+  maxCircles = 2000;
+  circleCount = 100;
+  circles = [];
+  for (let i = 0; i < circleCount; i++) {
+    createCircle();
+  }
+}
+
+
+
+/**
+ * DRAW FUNCTION RUNS CONTINUOUSLY AFTER SETUP
+ * - Clear the screen
+ * - Show the circles
+ * - Stop animating if needed
+ **/
+function draw() {
+
+  // Clear the screen
+  background(0);
+
+  // Show all circles
+  for (let c of circles) {
+    c.update(circles);
+    c.show();
+  }
+
+  // Stop animating
+  if (circles.length >= maxCircles) {
+    noLoop();
+    console.log("Too many circles");
+  }
+
+  // image(catImg, catOffset.x, catOffset.y);
+}
+
+
+
+/**
+ * DEFINE USEFUL FUNCTIONS FOR THE PROGRAM
+ * - Make a new circle
+ **/
+function createCircle() {
+  let valid = false;
+  let attempts = 0;
+  pos = createVector();
+  while (!valid) {
+    valid = true;
+    pos = createVector(random(catImg.width), random(catImg.height));
+    attempts++;
+    for (let c of circles) {
+      if (dist(pos.x, pos.y, c.position.x, c.position.y) <= c.radius) {
+        valid = false;
+        break;
+      }
+    }
+    if (attempts == maxAttempts) {
+      console.log("Too many attempts");
+      noLoop();
+      return;
+    }
+  }
+  // let c = noiseField[floor(pos.y/nSize)][floor(pos.x/nSize)];
+  // c = color(map(c, 0, 1, 0, 360), 100, 100);
+  let c = catImg.get(pos.x, pos.y);
+  pos.add(catOffset);
+  circles.push(new Blob(pos.copy(), c));
+}
